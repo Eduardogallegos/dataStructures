6388187ad47b49a7f269caa93399d03270f04a71
A01745776 <A01745776@itesm.mx>
2018-08-20 11:13:18 -0500

    Ejercicio en clase addFirst

--- src/mx/itesm/main/Main.java
+++ src/mx/itesm/main/Main.java
@@ -3,14 +3,52 @@ package mx.itesm.main;
 import mx.itesm.util.IntegerNode;
 
 public class Main {
+	
+	public static String toString(IntegerNode header) {
+		IntegerNode currentNode = header.next;
+		
+		if (currentNode == header) {
+			return "[]";
+		}
+		
+		String returnValue = "[" + currentNode.value;
+		while (currentNode.next != header) {
+			currentNode = currentNode.next;
+			returnValue += ", " + currentNode.value;
+		}
+		
+		returnValue += "]";
+		return returnValue;
+	}
 
 	public static void main(String[] args) {
 		IntegerNode sentinel = new IntegerNode();
 		IntegerNode cinco = new IntegerNode(5);
+		IntegerNode cuatro = new IntegerNode(4);
 		sentinel.next = cinco;
 		sentinel.prev = cinco;
 		cinco.next = sentinel;
 		cinco.prev = sentinel;
+		IntegerNode current = sentinel.next;
+		cuatro.next = current;
+		cuatro.prev = sentinel;
+		current.prev = cuatro;
+		sentinel.next = cuatro;
+		
+		addFirst(sentinel,7);
+		addFirst(sentinel,1);
+		
+		System.out.println(toString(sentinel));
+		
+	}
+	
+	public static void addFirst (IntegerNode header, int value) {
+		IntegerNode nuevo = new IntegerNode(value);
+		IntegerNode current = header.next;
+		nuevo.next = current;
+		nuevo.prev = header;
+		current.prev = nuevo;
+		header.next = nuevo;
 	}
 
 }
