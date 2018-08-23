37fe7e3efef59f37d5f82f535110d07fd051dfbc
A01745776 <A01745776@itesm.mx>
2018-08-23 10:54:04 -0500

    Node corregido

--- src/mx/itesm/util/Node.java
+++ src/mx/itesm/util/Node.java
@@ -1,5 +1,21 @@
 package mx.itesm.util;
 
-public class Node {
+public class Node<T> {
+
+	public T t;
+	public Node<T> next;
+	public Node<T> prev;
+
+	public Node() {
+		t = null;
+		next = this;
+		prev = this;
+	}
+
+	public Node(T t) {
+		this.t = t;
+		next = this;
+		prev = this;
+	}
 
 }
