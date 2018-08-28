8e974174d9e95db47909a663a991c0cd6f11663c
A01745776 <A01745776@itesm.mx>
2018-08-28 15:39:31 -0500

    Tarea 3 Listas Ligadas

--- src/mx/itesm/main/Main.java
+++ src/mx/itesm/main/Main.java
@@ -1,53 +1,56 @@
 package mx.itesm.main;
 
-import mx.itesm.util.IntegerNode;
+import mx.itesm.util.LinkedList;
 
 public class Main {
 
-	public static String toString(IntegerNode header) {
-		IntegerNode currentNode = header.next;
-
-		if (currentNode == header) {
-			return "[]";
-		}
-
-		String returnValue = "[" + currentNode.value;
-		while (currentNode.next != header) {
-			currentNode = currentNode.next;
-			returnValue += ", " + currentNode.value;
-		}
-
-		returnValue += "]";
-		return returnValue;
-	}
-
+	/*
+	 * public static String toString(IntegerNode header) { IntegerNode currentNode =
+	 * header.next;
+	 * 
+	 * if (currentNode == header) { return "[]"; }
+	 * 
+	 * String returnValue = "[" + currentNode.value; while (currentNode.next !=
+	 * header) { currentNode = currentNode.next; returnValue += ", " +
+	 * currentNode.value; }
+	 * 
+	 * returnValue += "]"; return returnValue; }
+	 * 
+	 * public static void main(String[] args) { IntegerNode sentinel = new
+	 * IntegerNode(); IntegerNode cinco = new IntegerNode(5); IntegerNode cuatro =
+	 * new IntegerNode(4); sentinel.next = cinco; sentinel.prev = cinco; cinco.next
+	 * = sentinel; cinco.prev = sentinel; IntegerNode current = sentinel.next;
+	 * cuatro.next = current; cuatro.prev = sentinel; current.prev = cuatro;
+	 * sentinel.next = cuatro;
+	 * 
+	 * addFirst(sentinel, 7); addFirst(sentinel, 1);
+	 * System.out.println(toString(sentinel));
+	 * 
+	 * }
+	 * 
+	 * public static void addFirst(IntegerNode header, int value) { IntegerNode
+	 * nuevo = new IntegerNode(value); IntegerNode current = header.next; nuevo.next
+	 * = current; nuevo.prev = header; current.prev = nuevo; header.next = nuevo; }
+	 */
 	public static void main(String[] args) {
-		IntegerNode sentinel = new IntegerNode();
-		IntegerNode cinco = new IntegerNode(5);
-		IntegerNode cuatro = new IntegerNode(4);
-		sentinel.next = cinco;
-		sentinel.prev = cinco;
-		cinco.next = sentinel;
-		cinco.prev = sentinel;
-		IntegerNode current = sentinel.next;
-		cuatro.next = current;
-		cuatro.prev = sentinel;
-		current.prev = cuatro;
-		sentinel.next = cuatro;
-
-		addFirst(sentinel, 7);
-		addFirst(sentinel, 1);
-		System.out.println(toString(sentinel));
-
+		LinkedList<Integer> integerList = new LinkedList<Integer>();
+		integerList.addFirst(7);
+		integerList.addFirst(3);
+		integerList.addLast(8);
+		integerList.addLast(6);
+		integerList.addLast(4);
+		integerList.addLast(5);
+		System.out.println(integerList);
+		System.out.println(integerList.toStringReverse());
+		integerList.add(2, 29);
+		System.out.println(integerList);
+		integerList.set(2,9);
+		System.out.println(integerList);
+		integerList.removeFirst();
+		System.out.println(integerList);
+		integerList.removeLast();
+		System.out.println(integerList);
+		integerList.remove(2);
+		System.out.println(integerList);
 	}
-
-	public static void addFirst(IntegerNode header, int value) {
-		IntegerNode nuevo = new IntegerNode(value);
-		IntegerNode current = header.next;
-		nuevo.next = current;
-		nuevo.prev = header;
-		current.prev = nuevo;
-		header.next = nuevo;
-	}
-
 }
--- src/mx/itesm/util/LinkedList.java
+++ src/mx/itesm/util/LinkedList.java
@@ -1,48 +1,165 @@
 package mx.itesm.util;
 
 import java.util.AbstractList;
+import java.util.NoSuchElementException;
 
 public class LinkedList<E> extends AbstractList<E> {
 
 	private Node<E> header;
 	private int size;
-	
+
 	public LinkedList() {
 		header = new Node<E>();
+		size = 0;
 	}
-	
+
 	public void addFirst(E element) {
-		Node <E> nuevo = new Node <E> (element);
-		Node <E> current = header.next;
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		Node<E> nuevo = new Node<E>(element);
+		Node<E> current = header.next;
 		nuevo.next = current;
 		nuevo.prev = header;
 		current.prev = nuevo;
 		header.next = nuevo;
-		size += 1;
+		size++;
 	}
-	
+
 	public void addLast(E element) {
-		Node <E> nuevo = new Node <E> (element);
-		Node <E> current = header.prev;
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		Node<E> nuevo = new Node<E>(element);
+		Node<E> current = header.prev;
 		nuevo.next = header;
 		nuevo.prev = current;
 		current.next = nuevo;
 		header.prev = nuevo;
-		size += 1;
+		size++;
 	}
-	
+
+	/**
+	 * Gets the node at the specified index.
+	 * 
+	 * @param index of the node to get
+	 * @return the node at the specified position
+	 * @throws IndexOutOfBoundException if the index is out of range (index < 0 ||
+	 *                                  index >= size())
+	 */
+	private Node<E> node(int index) {
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException("Index out of range");
+		}
+
+		Node<E> currentNode = header.next;
+		for (int i = 0; i < index; i++) {
+			currentNode = currentNode.next;
+		}
+
+		return currentNode;
+	}
+
 	@Override
-	public E get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public E get(int index) {
+		return node(index).t;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
-	
-	
+
+	public String toStringReverse() {
+		Node<E> currentNode = header.prev;
+		if (currentNode == header) {
+			return "[]";
+		}
+		String returnValue = "[" + currentNode.t;
+		while (currentNode.prev != header) {
+			currentNode = currentNode.prev;
+			returnValue += ", " + currentNode.t;
+		}
+		returnValue += "]";
+		return returnValue;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		Node<E> nuevo = new Node<E>(element);
+		Node<E> current = node(index);
+		Node<E> anterior = current.prev;
+		nuevo.next = current;
+		nuevo.prev = anterior;
+		current.prev = nuevo;
+		anterior.next = nuevo;
+		size++;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		Node<E> elementoNuevo = node(index);
+		elementoNuevo.t = element;
+		return elementoNuevo.t;
+	}
+
+	/**
+	 * Removes and returns the first element from this list.
+	 * 
+	 * @return the first element from this list
+	 * @throws NoSuchElementException if this list is empty
+	 */
+	public E removeFirst() {
+		if (size == 0) {
+			throw new NoSuchElementException();
+		}
+		Node<E> primero = header.next;
+		Node<E> nuevoPrimero = primero.next;
+		nuevoPrimero.prev = header;
+		header.next = nuevoPrimero;
+		primero.next = null;
+		primero.prev = null;
+		size--;
+		return nuevoPrimero.t;
+	}
+
+	/**
+	 * Removes and returns the last element from this list.
+	 * 
+	 * @return the last element from this list
+	 * @throws NoSuchElementException if this list is empty
+	 */
+	public E removeLast() {
+		if (size == 0) {
+			throw new NoSuchElementException();
+		}
+		Node<E> ultimo = header.prev;
+		Node<E> nuevoUltimo = ultimo.prev;
+		nuevoUltimo.next = header;
+		header.prev = nuevoUltimo;
+		ultimo.next = null;
+		ultimo.prev = null;
+		size--;
+		return nuevoUltimo.t;
+	}
+
+	@Override
+	public E remove(int index) {
+		Node<E> nodeToRemove = node(index);
+		Node<E> prevNode = nodeToRemove.prev;
+		Node<E> nextNode = nodeToRemove.next;
+		prevNode.next = nextNode;
+		nextNode.prev = prevNode;
+		nodeToRemove.next = null;
+		nodeToRemove.prev = null;
+		size--;
+		return nextNode.t;
+	}
 
 }
