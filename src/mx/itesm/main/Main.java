023e8e6427375da09a16623a93f12f903deb7c0e
A01745776 <A01745776@itesm.mx>
2018-10-04 10:23:55 -0500

    Recursive Algorithms

--- src/mx/itesm/main/Main.java
+++ src/mx/itesm/main/Main.java
@@ -1,59 +1,13 @@
 package mx.itesm.main;
 
-import mx.itesm.util.LinkedList;
-import mx.itesm.exercises.LinkedListApplications;
+import mx.itesm.recursion.Fibonacci;;
 
 public class Main {
 
-	/*
-	 * public static String toString(IntegerNode header) { IntegerNode currentNode =
-	 * header.next;
-	 * 
-	 * if (currentNode == header) { return "[]"; }
-	 * 
-	 * String returnValue = "[" + currentNode.value; while (currentNode.next !=
-	 * header) { currentNode = currentNode.next; returnValue += ", " +
-	 * currentNode.value; }
-	 * 
-	 * returnValue += "]"; return returnValue; }
-	 * 
-	 * public static void main(String[] args) { IntegerNode sentinel = new
-	 * IntegerNode(); IntegerNode cinco = new IntegerNode(5); IntegerNode cuatro =
-	 * new IntegerNode(4); sentinel.next = cinco; sentinel.prev = cinco; cinco.next
-	 * = sentinel; cinco.prev = sentinel; IntegerNode current = sentinel.next;
-	 * cuatro.next = current; cuatro.prev = sentinel; current.prev = cuatro;
-	 * sentinel.next = cuatro;
-	 * 
-	 * addFirst(sentinel, 7); addFirst(sentinel, 1);
-	 * System.out.println(toString(sentinel));
-	 * 
-	 * }
-	 * 
-	 * public static void addFirst(IntegerNode header, int value) { IntegerNode
-	 * nuevo = new IntegerNode(value); IntegerNode current = header.next; nuevo.next
-	 * = current; nuevo.prev = header; current.prev = nuevo; header.next = nuevo; }
-	 */
 	public static void main(String[] args) {
-		/*
-		 * LinkedList<Integer> integerList = new LinkedList<Integer>();
-		 * integerList.addFirst(7); integerList.addFirst(3); integerList.addLast(8);
-		 * integerList.addLast(6); integerList.addLast(4); integerList.addLast(5);
-		 * System.out.println(integerList);
-		 * System.out.println(integerList.toStringReverse()); integerList.add(2, 29);
-		 * System.out.println(integerList); integerList.set(2,9);
-		 * System.out.println(integerList); integerList.removeFirst();
-		 * System.out.println(integerList); integerList.removeLast();
-		 * System.out.println(integerList); integerList.remove(2);
-		 * System.out.println(integerList);
-		 */
-		LinkedListApplications a = new LinkedListApplications();
-		System.out.println(a.isPalindrome("1111"));
-		System.out.println(a.jumbleLetters("tank"));
-		System.out.println(a.jumbleLetters("tlaxcala"));
-		LinkedList<String> students = new LinkedList<String>();
-		LinkedList<String> stringList = new LinkedList<String>("Peeta", "Gale", "Haymitch", "Primrose", "Effie");
-
-		System.out.println(a.sortingHat(stringList, 7));
-
+		Fibonacci fib = new Fibonacci();
+		for(int i = 0; i < 100; i++) {
+			System.out.println(i + ".-" + fib.getNumber(i));
+		}
 	}
 }
---
+++ src/mx/itesm/recursion/Fibonacci.java
@@ -0,0 +1,24 @@
+package mx.itesm.recursion;
+
+public class Fibonacci {
+	
+	private int[] fibs;
+	private int maxN;
+	
+	public Fibonacci() {
+		fibs = new int[100];
+		fibs[0] = 0;
+		fibs[1] = 1;
+		maxN = 1;
+	}
+	
+	public int getNumber(int n) {
+		if (maxN < n) {
+			fibs[n] = getNumber(n-1) + getNumber(n-2);
+			maxN = n;
+		}
+		
+		return fibs[n];
+	}
+
+}
